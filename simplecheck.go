6e88a9b8cf67360419811236612c9580e0a4a22e
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-09-26 18:24:33 +0200

    Fixed formatting — string concatenation — in simplecheck

--- simplecheck.go
+++ simplecheck.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/ArchieT/trmstac/get"
 	"github.com/ArchieT/trmstac/stadata"
+	"strconv"
 )
 
 func main(){
@@ -11,15 +13,25 @@ func main(){
 //	fmt.Println(a)
 	fmt.Println("Liczba rowerów na stacjach TRM: ", a.Cza)
 	for ib,b := range a.Stali {
-		fmt.Print(" ", stadata.List[ib].Stastr, " | ")
+		var buffer bytes.Buffer
+		buffer.WriteString(" ")
+		buffer.WriteString(stadata.List[ib].Stastr)
+		buffer.WriteString(" | ")
 		for i:=b.Row;i>0;i-- {
 			//fmt.Print("█")
-			fmt.Print("▉")
+			buffer.WriteString("▉")
 		}
 		for i:=b.Wol;i>0;i-- {
-			fmt.Print("▒")
+			buffer.WriteString("▒")
 		}
-		fmt.Println(" ",b.Row,"/",b.Row+b.Wol,"(",b.Wol," empty)")
+		buffer.WriteString(" ")
+		buffer.WriteString(strconv.Itoa(b.Row))
+		buffer.WriteString("/")
+		buffer.WriteString(strconv.Itoa(b.Row+b.Wol))
+		buffer.WriteString(" (")
+		buffer.WriteString(strconv.Itoa(b.Wol))
+		buffer.WriteString(" empty)")
+		fmt.Println(buffer.String())
 	}
 	fmt.Println(" ")
 }
