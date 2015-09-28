f212af4f08d34cb37550ba0b3f5dbcf78fa5ac51
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-09-28 20:08:51 +0200

    Added sum to simplecheck

--- trmsimplecheck/simplecheck.go
+++ trmsimplecheck/simplecheck.go
@@ -12,6 +12,7 @@ func main(){
 	a := get.Download()
 //	fmt.Println(a)
 	fmt.Println("Liczba rowerów na stacjach TRM: ", a.Cza)
+	var sumrow,sumwol int
 	for ib,b := range a.Stali {
 		var buffer bytes.Buffer
 		buffer.WriteString(" ")
@@ -32,6 +33,30 @@ func main(){
 		buffer.WriteString(strconv.Itoa(b.Wol))
 		buffer.WriteString(" empty)")
 		fmt.Println(buffer.String())
+		sumrow+=b.Row
+		sumwol+=b.Wol
 	}
+	fmt.Println("----")
+	var buffer bytes.Buffer
+	buffer.WriteString(" SUMA   | ")
+	lproc:=(80*sumrow)/(sumrow+sumwol)
+	for i:=lproc;i>0;i-- {
+		buffer.WriteString("█")
+	}
+	for i:=(80-lproc);i>0;i-- {
+		buffer.WriteString("▒")
+	}
+	buffer.WriteString(" ")
+	buffer.WriteString(strconv.Itoa(sumrow))
+	buffer.WriteString("/")
+	buffer.WriteString(strconv.Itoa(sumrow+sumwol))
+	buffer.WriteString(" (")
+	buffer.WriteString(strconv.Itoa(sumwol))
+	buffer.WriteString(") — AVG ")
+	buffer.WriteString(strconv.Itoa(sumrow/len(a.Stali)))
+	buffer.WriteString(" (")
+	buffer.WriteString(strconv.Itoa(sumwol/len(a.Stali)))
+	buffer.WriteString(") ")
+	fmt.Println(buffer.String())
 	fmt.Println(" ")
 }
