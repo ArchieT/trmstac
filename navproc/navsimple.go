91df6b42e7b9c38c56242c423e1f33bc1142c9cc
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-10-23 14:30:34 +0200

    Working on navproc

--- navproc/navsimple.go
+++ navproc/navsimple.go
@@ -8,7 +8,7 @@ type FTStaWFunc func(*FTStaEntrInterface) uint16
 
 type FTStaEntrInterfaceMatrix [27][27]FTStaEntrInterface
 
-func (g *FTStaEntrInterfaceMatrix) ShortestPath(w FTStaWFunc, from,to uint8) uint16 {
+func (g *FTStaEntrInterfaceMatrix) ShortestPath(w FTStaWFunc, from,to uint8) (uint16,[]uint8) {
 	var d [27]uint16
 	var p [27]uint8
 	q := make([]uint8, 27)
@@ -29,7 +29,20 @@ func (g *FTStaEntrInterfaceMatrix) ShortestPath(w FTStaWFunc, from,to uint8) uin
 			}
 		}
 	}
-	return d[to]
+	poprzednicy:=make([]uint8,0,27)
+	niemafrom := true
+	poprzednicy = append(poprzednicy,to)
+	for niemafrom {
+		poprzednicy = append(poprzednicy,p[poprzednicy[len(poprzednicy)-1]])
+		if poprzednicy[len(poprzednicy)-1] == from {
+			niemafrom = false
+		}
+	}
+	for i := len(poprzednicy)/2-1; i >= 0; i-- {
+		opp := len(poprzednicy)-1-i
+		poprzednicy[i], poprzednicy[opp] = poprzednicy[opp], poprzednicy[i]
+	}
+	return d[to],poprzednicy
 }
 
 func TimeSec(i *FTStaEntrInterface) uint16 {
@@ -52,3 +65,5 @@ func Oplata(i *FTStaEntrInterface) uint16 {
 	}
 	return uint16(65535)
 }
+
+
