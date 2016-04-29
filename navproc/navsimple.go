c141fbd123531de46fe6cff0c585478d4f72e1e1
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-04-29 09:30:24 +0200

    Working on sta28

--- navproc/navsimple.go
+++ navproc/navsimple.go
@@ -1,48 +1,50 @@
 package navproc
 
+import "github.com/ArchieT/trmstac/stadata"
+
 type FTStaEntrInterface interface {
 	TimeSec() uint16
 }
 
 type FTStaWFunc func(FTStaEntrInterface) uint16
 
-//type FTStaEntrInterfaceMatrix [27][27]FTStaEntrInterface
+//type FTStaEntrInterfaceMatrix [stadata.ILOSCSTA][stadata.ILOSCSTA]FTStaEntrInterface
 
-func ShortestPath(g [27][27]FTStaEntrInterface, w FTStaWFunc, from,to uint8) (uint16,[]uint8) {
-	var d [27]uint16
-	var p [27]uint8
-	q := make([]uint8, 27)
+func ShortestPath(g [stadata.ILOSCSTA][stadata.ILOSCSTA]FTStaEntrInterface, w FTStaWFunc, from, to uint8) (uint16, []uint8) {
+	var d [stadata.ILOSCSTA]uint16
+	var p [stadata.ILOSCSTA]uint8
+	q := make([]uint8, stadata.ILOSCSTA)
 	var v uint8
-	for v=0;v<27;v++ {
+	for v = 0; v < stadata.ILOSCSTA; v++ {
 		d[v] = ^uint16(0)
 		p[v] = ^uint8(0)
 		q[v] = v
 	}
 	d[from] = 0
-	for len(q)>0 {
-		u,q := q[0],q[:1]
-		for v=0;v<27;v++ {
-			if d[v] > d[u] + w((g[u][v])) {
+	for len(q) > 0 {
+		u, q := q[0], q[:1]
+		for v = 0; v < stadata.ILOSCSTA; v++ {
+			if d[v] > d[u]+w((g[u][v])) {
 				d[v] = d[u] + w((g[u][v]))
 				p[v] = u
-				q = append(q,v)
+				q = append(q, v)
 			}
 		}
 	}
-	poprzednicy:=make([]uint8,0,27)
+	poprzednicy := make([]uint8, 0, stadata.ILOSCSTA)
 	niemafrom := true
-	poprzednicy = append(poprzednicy,to)
+	poprzednicy = append(poprzednicy, to)
 	for niemafrom {
-		poprzednicy = append(poprzednicy,p[poprzednicy[len(poprzednicy)-1]])
+		poprzednicy = append(poprzednicy, p[poprzednicy[len(poprzednicy)-1]])
 		if poprzednicy[len(poprzednicy)-1] == from {
 			niemafrom = false
 		}
 	}
-	for i := len(poprzednicy)/2-1; i >= 0; i-- {
-		opp := len(poprzednicy)-1-i
+	for i := len(poprzednicy)/2 - 1; i >= 0; i-- {
+		opp := len(poprzednicy) - 1 - i
 		poprzednicy[i], poprzednicy[opp] = poprzednicy[opp], poprzednicy[i]
 	}
-	return d[to],poprzednicy
+	return d[to], poprzednicy
 }
 
 func TimeSec(i FTStaEntrInterface) uint16 {
@@ -52,18 +54,16 @@ func TimeSec(i FTStaEntrInterface) uint16 {
 func Oplata(i FTStaEntrInterface) uint16 {
 	if i.TimeSec() < 20 {
 		return uint16(0)
-	} else if i.TimeSec() <60 {
+	} else if i.TimeSec() < 60 {
 		return uint16(1)
 	} else if i.TimeSec() < 120 {
 		return uint16(4)
 	} else if i.TimeSec() < 180 {
 		return uint16(9)
-	} else if i.TimeSec() <240 {
+	} else if i.TimeSec() < 240 {
 		return uint16(16)
 	} else if i.TimeSec() > 239 {
-		return uint16((9+((i.TimeSec()-179)/60)+1)*7)
+		return uint16((9 + ((i.TimeSec() - 179) / 60) + 1) * 7)
 	}
 	return uint16(65535)
 }
-
-
